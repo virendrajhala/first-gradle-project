c911919667c7855c0b1ce22ffae172f8dd8ea756
Virendra Jhala <virendrapratapsinghjhala@gmail.com>
2021-09-23 16:43:39 +0530

    Added Application.kt and Resource.kt

---
+++ src/main/java/Application.kt
@@ -0,0 +1,15 @@
+import jakarta.ws.rs.core.UriBuilder
+import org.glassfish.grizzly.http.server.HttpServer
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
+import org.glassfish.jersey.server.ResourceConfig
+import serveresource.Resource
+import java.net.URI
+
+
+fun main(){
+
+    val baseUri:URI = UriBuilder.fromUri("http://localhost/").port(8080).build()
+    val config:ResourceConfig = ResourceConfig(Resource::class.java)
+    val server = GrizzlyHttpServerFactory.createHttpServer(baseUri,config)
+}
+
---
+++ src/main/java/serveresource/Resource.kt
@@ -0,0 +1,17 @@
+package serveresource
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+
+@Path("/serveresource")
+class Resource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getServerStatus() : Response{
+      return Response.ok("Welcome").build()
+    }
+
+}
